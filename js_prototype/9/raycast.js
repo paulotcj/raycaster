2fe71b4e4acad673b3989df0360001c7e4aa4781
Paulo Costa <paulotcj@gmail.com>
2022-12-03 15:07:48 -0500

    .

--- js_prototype/9/raycast.js
+++ js_prototype/9/raycast.js
@@ -353,7 +353,11 @@ function render3DProjectedWalls()
     {
         var ray = rays[i];
 
-        var rayDistance = ray.distance;
+        // get the perpendicular distance to the wall to fix fishbowl distortion
+        var correctWallDistance = ray.distance * Math.cos( ray.rayAngle - player.rotationAngle);
+        
+        // enable this and comment the code above to enable fishbowl view
+        //var rayDistance = correctWallDistance;
 
         // calculate the distance to the projection plane
         //   note: we are trying to calculate the distance or the adjacent side of a 90deg triangle
@@ -365,7 +369,7 @@ function render3DProjectedWalls()
         //  the equality formula is: 'actual wall height'/'distance to wall' = 'projected wall height'/'distance form player to proj. plane'
         //  since we want to find 'projected wall height', we reorganize the equation to:
         //   'projected wall height' = 'actual wall height'/'distance to wall' * 'distance form player to proj. plane'
-        var wallStripHeight = (TILE_SIZE / rayDistance) * distanceProjectionPlane;
+        var wallStripHeight = (TILE_SIZE / correctWallDistance) * distanceProjectionPlane;
 
         fill("rgba(255, 255, 255, 1.0)");
         noStroke();
