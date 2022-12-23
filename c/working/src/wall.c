2ca6addcea1cdb65b66a228a76a7a8b97a19f30e
Paulo <Costa>
2022-12-23 03:44:53 -0500

    .

--- c/working/src/wall.c
+++ c/working/src/wall.c
@@ -1,5 +1,15 @@
 #include "wall.h"
 
+// Change the color intensity based on a factor value between 0 and 1
+void changeColorIntensity(color_t* color, float factor) {
+    color_t a = (*color & 0xFF000000);
+    color_t r = (*color & 0x00FF0000) * factor;
+    color_t g = (*color & 0x0000FF00) * factor;
+    color_t b = (*color & 0x000000FF) * factor;
+
+    *color = a | (r & 0x00FF0000) | (g & 0x0000FF00) | (b & 0x000000FF);
+}
+
 void renderWallProjection(void) 
 {
     for (int x = 0; x < NUM_RAYS; x++) 
@@ -129,6 +139,14 @@ void renderWallProjection(void)
             
             // set the color of the wall based on the color from the texture
             color_t texelColor = wallTextures[texNum].texture_buffer[(texture_width * textureOffsetY) + textureOffsetX];
+            
+            // Make the pixel color darker if the ray hit was vertical
+            if (rays[x].wasHitVertical) {
+                changeColorIntensity(&texelColor, 0.7);
+            }            
+            
+            
+            
             drawPixel(x, y, texelColor);
         }
 
